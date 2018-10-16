0a3204b4b43065b5520246fa88743f111e974719
Ryan Berdeen <rberdeen@hubspot.com>
2018-10-16 17:54:04 -0400

    add a simple test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,8 @@ module.exports = {
   parser: 'babel-eslint',
   extends: 'eslint:recommended',
   env: { node: true, es6: true },
+  overrides: {
+    files: ['**/__tests__/**/*.js'],
+    env: { jest: true },
+  },
 };
