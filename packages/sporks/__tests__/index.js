e095786be30eca790054cc0f5b347ef9916659b3
Ryan Berdeen <ryan@ryanberdeen.com>
2018-10-16 18:41:15 -0400

    add a simple test (#1)

---
+++ packages/sporks/__tests__/index.js
@@ -0,0 +1,24 @@
+import { parse } from '../src';
+
+describe('parse', () => {
+  it('parses', () => {
+    expect(parse('#= hello world')).toMatchInlineSnapshot(`
+Object {
+  "directives": Array [
+    Object {
+      "args": Array [
+        "world",
+      ],
+      "directive": "hello",
+      "loc": Object {
+        "start": 0,
+      },
+      "source": "#= hello world",
+    },
+  ],
+  "headerSource": "#= hello world",
+  "source": "",
+}
+`);
+  });
+});
