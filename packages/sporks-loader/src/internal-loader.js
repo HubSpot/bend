acc3995a15f7d078b75cd2d977cb6d5c3ed099cc
Gus Vargas <gusvargas@gmail.com>
2019-04-05 15:54:45 -0400

    Remove use of OptionalParsePlugin (#21)

--- packages/sporks-loader/src/internal-loader.js
+++ packages/sporks-loader/src/internal-loader.js
@@ -1,9 +1,5 @@
 module.exports = function(...args) {
-  if (!this.noParse) {
-    throw new Error('missing OptionalParsePlugin');
-  }
   this.cacheable();
-  this.noParse();
   this._module.useSourceMap = true;
   this.callback(null, ...args);
 };
