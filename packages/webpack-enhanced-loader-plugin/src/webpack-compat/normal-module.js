8722196cedf4ce98f9ed2d374f45b4769b7ed0c0
hdineen <hdineen@hubspot.com>
2021-09-15 15:53:29 -0400

    remove some weback3 specific logic

--- packages/webpack-enhanced-loader-plugin/src/webpack-compat/normal-module.js
+++ packages/webpack-enhanced-loader-plugin/src/webpack-compat/normal-module.js
@@ -1,5 +1,3 @@
 export function buildInfo(mod) {
-  // in webpack 3, buildInfo like `fileDependencies` was set directly on the module.
-  // in webpack 4, it is set on the `buildInfo` object
-  return mod.buildInfo ? mod.buildInfo : mod;
+  return mod.buildInfo;
 }
