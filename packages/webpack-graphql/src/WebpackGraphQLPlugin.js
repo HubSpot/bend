587b3c30a23da6c259ca2413a1ae88fe7f91d0a1
bbaum <bbaum@hubspot.com>
2025-10-17 11:05:55 -0400

    update dev server plugin api

--- packages/webpack-graphql/src/WebpackGraphQLPlugin.js
+++ packages/webpack-graphql/src/WebpackGraphQLPlugin.js
@@ -6,11 +6,11 @@ export default class WebpackGraphQLPlugin extends WebpackDevServerPlugin {
     this.opts = opts;
   }
 
-  setupMiddlewares(middlewares, devServer, compiler) {
+  setupMiddlewares(middlewares, server, compiler) {
     const { path, context, typeDefs, resolvers } = this.opts;
     const { buildContext, makeSchema } = require('./data');
     const graphqlHTTP = require('express-graphql');
-    const app = devServer.app;
+    const app = server.app;
 
     app.use(
       path,
